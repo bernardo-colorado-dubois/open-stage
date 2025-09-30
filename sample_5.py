55cf20c10f2bbba5ff7dc2832e1f04710cf4e786
bernardo-colorado-dubois <bernardocoloradodubois.fsd@gmail.com>
2025-09-30 10:23:36 -0600

    agregando ejemplos

--- sample_5.py
+++ sample_5.py
@@ -1,5 +1,5 @@
 import os
-from src.core import Pipe, Printer, GCPBigQueryOrigin,AnthropicPromptTransformer
+from src.core import Pipe, Printer, GCPBigQueryOrigin
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,19 +16,8 @@ bigquery_origin = GCPBigQueryOrigin(
 
 bigquery_pipe = Pipe(name="bigquery_pipe")
 
-anthropic_transformer = AnthropicPromptTransformer(
-  name="anthropic_transformer",
-  api_key=ANTHROPIC_API_KEY,
-  prompt="Provide only the setosa  species, with a petal length less or equal than 0.2 from the following data, and drop the sepal columns also show the species in lower case",
-  model="claude-sonnet-4-5-20250929"
-)
+bigquery_printer = Printer(name="bigquery_printer")
 
-anthropic_pipe = Pipe(name="anthropic_pipe")
-
-anthropic_printer = Printer(name="anthropic_printer")
-
-bigquery_origin.add_output(bigquery_pipe).set_destination(anthropic_transformer)
-
-anthropic_transformer.add_output(anthropic_pipe).set_destination(anthropic_printer)
+bigquery_origin.add_output(bigquery_pipe).set_destination(bigquery_printer)
 
 bigquery_origin.pump()
