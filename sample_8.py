579277db5667c38e1529191469a9f1b4386c2315
bernardo-colorado-dubois <bernardocoloradodubois.fsd@gmail.com>
2025-09-30 23:42:04 -0600

    probando filtro

---
+++ sample_8.py
@@ -0,0 +1,44 @@
+from src.core import CSVOrigin, Pipe, Printer, Filter
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+csv_iris = CSVOrigin(
+  name='csv_iris',filepath_or_buffer='./csv/iris.csv',
+  skiprows=1,
+  header=None,
+  names=[
+    'sepal_length', 
+    'sepal_width', 
+    'petal_length', 
+    'petal_width', 
+    'species'
+  ],
+  dtype={
+    'sepal_length': 'float64', 
+    'sepal_width': 'float64', 
+    'petal_length': 'float64', 
+    'petal_width': 'float64', 
+    'species': 'string'
+  }
+)
+
+iris_pipe = Pipe(name='iris_pipe')
+
+iris_filter = Filter(
+  name='iris_filter',
+  field='petal_length',
+  condition='between',
+  value_or_values=[1.0, 4.0]
+)
+
+iris_pipe_filtered = Pipe(name='iris_pipe')
+
+iris_printer = Printer(name='iris_printer')
+
+csv_iris.add_output(iris_pipe).set_destination(iris_filter)
+iris_filter.add_output(iris_pipe_filtered).set_destination(iris_printer)
+
+csv_iris.pump()
