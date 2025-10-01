53a0a1b539d2d646de58c4fb32068fb3569bfa72
bernardo-colorado-dubois <bernardocoloradodubois.fsd@gmail.com>
2025-09-30 18:18:41 -0600

    agregando ejemplo de postgres

--- sample_3.py
+++ sample_3.py
@@ -3,10 +3,7 @@ from src.core import Pipe, Printer, PostgresOrigin
 from dotenv import load_dotenv
 from urllib.parse import quote_plus
 
-
 load_dotenv()
-
-credentials_path = "/home/berni-boom/qualitasfraude-8be2cf5b3f4b.json"
 
 PSQL_DB_HOST = os.getenv("PSQL_DB_HOST")
 PSQL_DB_PORT = int(os.getenv("PSQL_DB_PORT"))
@@ -15,6 +12,7 @@ PSQL_DB_USER = os.getenv("PSQL_DB_USER")
 PSQL_DB_PASSWORD = quote_plus(os.getenv("PSQL_DB_PASSWORD"))
 
 postgres_origin = PostgresOrigin(
+  name="postgres_origin",
   host=PSQL_DB_HOST,
   port=PSQL_DB_PORT,
   database=PSQL_DB_NAME,
@@ -28,4 +26,6 @@ postgres_pipe = Pipe(name="postgres_pipe")
 postgres_printer = Printer(name="postgres_printer")
 
 postgres_origin.add_output(postgres_pipe).set_destination(postgres_printer)
+
+postgres_origin.pump()
 
