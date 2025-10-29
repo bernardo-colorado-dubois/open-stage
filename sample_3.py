3b7e97b92b656d7b9fd31150357bfb9aabd864a1
bernardo-colorado-dubois <Bernardo Colorado Dubois>
2025-10-29 09:45:35 -0600

    prueba de conector mejorado de postgres y mysql

--- sample_3.py
+++ sample_3.py
@@ -5,6 +5,7 @@ from urllib.parse import quote_plus
 from src.core.base import Pipe
 from src.core.common import  Printer,DeleteColumns
 from src.postgres.common import PostgresOrigin
+from src.mysql.common import MySQLOrigin
 
 load_dotenv()
 
@@ -14,7 +15,13 @@ PSQL_DB_NAME = os.getenv("PSQL_DB_NAME")
 PSQL_DB_USER = os.getenv("PSQL_DB_USER")
 PSQL_DB_PASSWORD = quote_plus(os.getenv("PSQL_DB_PASSWORD"))
 
-postgres_origin = PostgresOrigin(
+MYSQL_DB_HOST=os.getenv("MYSQL_DB_HOST")
+MYSQL_DB_PORT=int(os.getenv("MYSQL_DB_PORT"))
+MYSQL_DB_NAME=os.getenv("MYSQL_DB_NAME")
+MYSQL_DB_USER=os.getenv("MYSQL_DB_USER")
+MYSQL_DB_PASSWORD=os.getenv("MYSQL_DB_PASSWORD")
+
+productos_origin = PostgresOrigin(
   name="postgres_origin",
   host=PSQL_DB_HOST,
   port=PSQL_DB_PORT,
@@ -29,7 +36,25 @@ productos_deleter = DeleteColumns(
   columns=["descripcion","imagen_url","proveedor_id","stock_actual","stock_minimo","fecha_caducidad","imagen_url","activo","created_at","updated_at"]
 )
 
+categorias_origin = MySQLOrigin(
+  name="mysql_origin",
+  host=MYSQL_DB_HOST,
+  port=MYSQL_DB_PORT,
+  database=MYSQL_DB_NAME,
+  user=MYSQL_DB_USER,
+  password=MYSQL_DB_PASSWORD,
+  table="categorias"
+)
 
-postgres_origin.add_output_pipe(Pipe(name="productos")).set_destination(productos_deleter).add_output_pipe(Pipe(name="prouctos_reducidos")).set_destination(Printer(name="productos_printer"))
-postgres_origin.pump()
+categorias_deleter = DeleteColumns(
+  name="categorias_deleter",
+  columns=["descripcion","created_at","updated_at","activo"]
+)
 
+productos_origin.add_output_pipe(Pipe(name="productos")).set_destination(productos_deleter).add_output_pipe(Pipe(name="prouctos_reducidos")).set_destination(Printer(name="productos_printer"))
+
+categorias_origin.add_output_pipe(Pipe(name="categorias")).set_destination(categorias_deleter).add_output_pipe(Pipe(name="categorias_reducidas")).set_destination(Printer(name="categorias_printer"))
+
+productos_origin.pump()
+
+categorias_origin.pump()
